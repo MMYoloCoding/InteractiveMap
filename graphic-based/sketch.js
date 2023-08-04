5a1b633bb589036145434e51e2f399d48623e9db
MMYoloCoding <michael223479@gmail.com>
2023-08-04 13:21:41 +0800

    nothing

--- graphic-based/sketch.js
+++ graphic-based/sketch.js
@@ -1,7 +1,17 @@
+
+let drawButton;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  background(220);   // Set the background color to gray
+  drawButton = createButton("Draw Zone");
 }
 
-function draw() {
-  background(220);
-}
+// function draw() {
+//   if (mouseIsPressed) {
+//     fill(255);
+//     ellipse(mouseX, mouseY, 8, 8);
+//   } else {
+//     fill(255);
+//   }
+  
+// }
